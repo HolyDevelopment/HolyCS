2504bf10b461449794c9500951e8d3f8a3a58d75
Yegg <43521928+Yegg12@users.noreply.github.com>
2018-12-23 20:20:58 +0100

    Add files via upload

---
+++ HolyCS/HolyCS/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HolyCS
+{
+
+    class Program
+    {
+        class God
+        {
+            static public void Say(string args)
+            {
+                Console.Write(args);
+            }
+            static public void Sayeth(string args)
+            {
+                Console.WriteLine(args + "eth");
+            }
+            static public string HearPrayer()
+            {
+                return(Console.ReadLine());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string Adam;
+
+            God.Sayeth("who is Adam");
+            Adam = God.HearPrayer();
+            God.Sayeth("You believe Adam is " + Adam + "?");
+        }
+    }
+}
