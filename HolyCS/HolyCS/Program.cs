757493a5b736a993d1eba8e217c5168f899bf1f5
Yegg12 <comicsanslolfuni@gmail.com>
2018-12-24 11:55:32 +0100

    made new namespace named Heaven, everything related to the programming language is no longer using namespace System, but instead namespace Heaven

--- HolyCS/HolyCS/Program.cs
+++ HolyCS/HolyCS/Program.cs
@@ -1,66 +1,71 @@
 using System;
+using Heaven;
+
+namespace Heaven
+{
+    class ConvertTo
+    {
+        static public short Hinduism(string text)
+        {
+            return Convert.ToInt16(text);
+        }
+        static public int Judaism(string text)
+        {
+            return Convert.ToInt32(text);
+        }
+        static public long Christianity(string text)
+        {
+            return Convert.ToInt64(text);
+        }
+    }
+    class God
+    {
+        static public void Say(string args)
+        {
+            Console.Write(args);
+        }
+        static public void Sayeth(string args)
+        {
+            Console.WriteLine(args.Replace(" ", "eth ").Replace(".", "eth.").Replace("?", "eth?").Replace("!", "eth!"));
+        }
+        static public void SayLine(string args)
+        {
+            Console.WriteLine(args);
+        }
+        static public void SayLine(int args)
+        {
+            Console.WriteLine(args);
+        }
+        static public void SayLine(bool args)
+        {
+            Console.WriteLine(args);
+        }
+        static public void SayLine(char args)
+        {
+            Console.WriteLine(args);
+        }
+        static public string ListenPrayer()
+        {
+            return Console.ReadLine();
+        }
+        static public int Listen()
+        {
+            return Console.Read();
+        }
+    }
+}
 
 namespace HolyCS
 {
+    
     class Program
     {
-        
-        
-
-        class ConvertTo
-        {
-            static public short Hinduism(string text)
-            {
-                return Convert.ToInt16(text);
-            }
-            static public int Judaism(string text)
-            {
-                return Convert.ToInt32(text);
-            }
-            static public long Christianity(string text)
-            {
-                return Convert.ToInt64(text);
-            }
-        }
-        class God
-        {
-            static public void Say(string args)
-            {
-                Console.Write(args);
-            }
-            static public void Sayeth(string args)
-            {
-                Console.WriteLine(args.Replace(" ", "eth ").Replace(".", "eth.").Replace("?", "eth?").Replace("!", "eth!"));
-            }
-            static public void SayLine(string args)
-            {
-                Console.WriteLine(args);
-            }
-            static public void SayLine(int args)
-            {
-                Console.WriteLine(args);
-            }
-            static public void SayLine(bool args)
-            {
-                Console.WriteLine(args);
-            }
-            static public void SayLine(char args)
-            {
-                Console.WriteLine(args);
-            }
-            static public string ListenPrayer()
-            {
-                return Console.ReadLine();
-            }
-            static public int Listen()
-            {
-                return Console.Read();
-            }
-        }
-
         static void Main(string[] args)
         {
-                God.Sayeth("You are retardese.");   
+            string say;
+
+            say = God.ListenPrayer();
+            God.Sayeth(say);
         }
     }
 }
