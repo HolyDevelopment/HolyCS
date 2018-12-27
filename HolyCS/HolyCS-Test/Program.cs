74fd0d50efe8eb30016e4785423cf91c16c62107
name <38593826+nonparallel@users.noreply.github.com>
2018-12-27 14:48:00 +0200

    literally fix the whole class library issue
    ok

---
+++ HolyCS/HolyCS-Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using HolyCS.Heaven;
+
+namespace HolyCS_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            God.SayLine("Hello Jesus!");
+            God.Listen();
+        }
+    }
+}
---
+++ HolyCS/HolyCS/Heaven/God.cs
@@ -0,0 +1,38 @@
+﻿namespace HolyCS.Heaven
+{
+    public class God
+    {
+        static public void Say(string args)
+        {
+            System.Console.Write(args);
+        }
+        static public void Sayeth(string args)
+        {
+            System.Console.WriteLine(args.Replace(" ", "eth ").Replace(".", "eth.").Replace("?", "eth?").Replace("!", "eth!"));
+        }
+        static public void SayLine(string args)
+        {
+            System.Console.WriteLine(args);
+        }
+        static public void SayLine(int args)
+        {
+            System.Console.WriteLine(args);
+        }
+        static public void SayLine(bool args)
+        {
+            System.Console.WriteLine(args);
+        }
+        static public void SayLine(char args)
+        {
+            System.Console.WriteLine(args);
+        }
+        static public string ListenPrayer()
+        {
+            return System.Console.ReadLine();
+        }
+        static public int Listen()
+        {
+            return System.Console.Read();
+        }
+    }
+}
