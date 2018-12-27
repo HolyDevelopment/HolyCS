a78f4fb8c4dfed02b56eb8a49637443d14060963
name <38593826+nonparallel@users.noreply.github.com>
2018-12-27 15:15:38 +0200

    add words

---
+++ HolyCS/HolyCS/Heaven/Words.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HolyCS.Heaven
+{
+    public class Words
+    {
+        static string Text;
+
+        public Words(string s) => Text = s;
+
+        public new string ToString() => Text;
+
+        public static implicit operator Words(string v)
+        {
+            return new Words(v);
+        }
+
+        public static explicit operator string(Words v)
+        {
+            return Text;
+        }
+    }
+}
