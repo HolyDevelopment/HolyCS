c8d0922bfea661374eb00aef07cee1978782e7f8
name <38593826+nonparallel@users.noreply.github.com>
2018-12-27 17:49:51 +0200

    CONfliCTS

--- HolyCS/HolyCS/Heaven/Convert.cs
+++ HolyCS/HolyCS/Heaven/Convert.cs
@@ -5,9 +5,5 @@ namespace HolyCS.Heaven
         static public short Hinduism(string text) => System.Convert.ToInt16(text);
         static public int Judaism(string text) => System.Convert.ToInt32(text);
         static public long Christianity(string text) => System.Convert.ToInt64(text);
-
-        static public short Hinduism(Words text) => System.Convert.ToInt16(text);
-        static public int Judaism(Words text) => System.Convert.ToInt32(text);
-        static public long Christianity(Words text) => System.Convert.ToInt64(text);
     }
 }
